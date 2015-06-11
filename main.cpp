d959cd77391a19e342148955b49f4e4718c00faf
projectc9 <itsyukai@gmail.com>
2015-06-11 14:12:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,122 +1,342 @@
-// Team 2
-#include <iostream>
+//
+//  main.cpp
+//  group ibrahim part
+//
+//  Created by ibrahim ibrahim on 6/9/15.
+//  Copyright (c) 2015 ibrahim ibrahim. All rights reserved.
+//
 
+#include <iostream>
+#include <string>
+#include<fstream>
+#include <stdlib.h>
+#include <cmath>
+#include "scrapper.h"
+#include "hash.h"
+#include "BinarySearchTree.h"
 using namespace std;
-void menu()
+
+
+
+
+void readinfile(HASH*, BinarySearchTree<Scrapper> *tree1Ptr);
+
+void seperatedata(string&, string&, int&, int&, int&, int&, string&, string&, string&);
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// TREE SHIT
+
+// display function to pass to BST traverse functions
+void display(Scrapper & anItem)
 {
-    cout <<"Menu"<<endl
-        <<"H - Hash" <<endl
-        <<"B - BST" <<endl
-        <<"Q - Quit"<<endl;
+    cout  << anItem << endl;
 }
-void menuBST()
+
+// display2 print indented list
+void display2(Scrapper & anItem, int&  num)
 {
-    cout <<"BST Menu:"<<endl
-        <<"1 - Display sorted by Primary Key(name)"<<endl
-        <<"2 - Display sorted by Secondary Key(height)"<<endl
-        <<"R - Remove object"<<endl
-        <<"S - Search"<<endl
-        <<"Q - Quit"<<endl;
-}
-void menuHash()
-{
-    cout <<"Hash Menu:"<<endl
-    <<"D - Displays all items"<<endl
-    <<"P - Prints an indented list of the hash table"<<endl
-    <<"R - Remove object"<<endl
-    <<"S - Search"<<endl
-    <<"Q - Quit"<<endl;
-}
-/*
-void print(hash * hash1)
-{
-    int hashSize = hash1->getHashSize();
- 
-    for(int i=0; i < hashSize; i++)
+    for (int x=0; x<num+2; x++)
     {
-        cout <<hash1[i] // if this is a linked list will have to write another loop to access
+        cout<<'\t';
     }
- 
+    
+    cout << num << ":" << anItem << endl;
+    
 }
-*/
+
+// check is used to check if value is found in tree
+void check(bool success)
+{
+    if (success)
+        cout << "found." << endl;
+    else
+        cout << " Entry not in tree." << endl;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void menu(HASH*);
+
+void menu_tree(BinarySearchTree<Scrapper> *tree1Ptr);
+
+
 int main()
 {
-    char choice;
-    menu();
+   
+    BinarySearchTree<Scrapper>* tree1Ptr= new BinarySearchTree<Scrapper>;
+    HASH* hash = new HASH(10);
+   
+    readinfile(hash,tree1Ptr);
     
-    // bst object bst1; // sorted by primary key
-    // bst object bst2; // sorted by secondary key
-    // hash object hash1;
-    // hash object * hashPtr = hash1;
-    // skyscraper ss1;
-    do
-    {
-        cin >> choice;
-        if (toupper(choice) == 'H')
-        {
-            menuHash();
-            do
-            {
-                cin>>choice;
-                if (toupper(choice) == 'D')
-                {
-                    //hash1.display(); // could write a function in main to display all the shit
-                    // display(hash1);
-                
-                }
-                else if (toupper(choice) == 'P')
-                {
-                    //print(hash1); // will write a function in main to print all the shit
-                
-                }
-                else if (toupper(choice) == 'R')
-                {
-                    //hash1.remove(x) // x could be a hash key or a string to be ocnverted into a hash
-                                      // by another hash member function
-                }
-                else if (toupper(choice) == 'S')
-                {
-                    //hash1.search(x) // once again x could be string or int
-                }
+    menu(hash);
+    menu_tree(tree1Ptr);
     
-            }while(toupper(choice) != 'Q');
-        
-        }// end Hash menu
-        
-        
-        else if(toupper(choice) == 'B')
-        {
-            menuBST();
-            do
-            {
-                cin >>choice;
-                if (choice == '1')
-                {
-                    // bst1.display() // Should only display Names per line
-                }
-                else if (choice == '2')
-                {
-                    // bst2.display()
-                }
-                else if (toupper(choice) == 'R')
-                {
-                    // bst1.remove(x);
-                }
-                else if (toupper(choice) == 'S')
-                {
-                    
-                    // ss1= bst1.search(x); // return obj
-                    // cout<<ss1;
-                    
-                    // bst2.search(x);
-                }
-            }while(toupper(choice) != 'Q');
-        } // end BST Menu
-            
-    }while (toupper(choice) != 'Q');
-
+    delete tree1Ptr;
+    delete hash;
     
-
-
     return 0;
 }
+
+void readinfile(HASH* hash,BinarySearchTree<Scrapper> *tree1Ptr)
+{
+    ifstream infile;
+    infile.open("tower.txt");
+    if(!infile)
+    {
+        cout << "No such file exists." << endl;
+        
+    }
+    string temp;
+    string na;
+    int flo = 0;
+    int yea = 0;
+    int ft = 0;
+    int m = 0;
+    string cit;
+    string coun;
+    string coor;
+    
+    while(!infile.eof())
+    {
+        getline(infile, temp);
+        
+        seperatedata(temp, na, flo, yea, ft, m, cit, coun, coor);
+        Scrapper data(na, ft, m, coun, flo, cit, yea, coor);
+        hash->add(data);
+        tree1Ptr->insert(data);
+    }
+ 
+    
+    infile.close();
+    
+}
+
+void seperatedata(string& temp, string& na, int& flo, int& yea, int& ft,
+                  int& m, string& cit, string& coun, string& coor)
+{
+    int i = 0;
+    string floor;
+    
+    na = temp.substr(0, temp.find(';')); // na = name
+    
+    i = i + na.length() + 1;
+    cit = temp.substr(i, temp.find(';', i) - i); // cit = city
+    
+    i = i + cit.length() + 1;
+    coun = temp.substr(i, temp.find(';', i) - i); // coun = country
+    
+    i = i + coun.length() + 1;
+    m = atoi(temp.substr(i, temp.find(';', i) - i).c_str()); // height in meters
+    
+    i = i + temp.substr(i, temp.find(';', i) - i).length() + 1;
+    ft = atoi(temp.substr(i, temp.find(';', i) - i).c_str()); // height in feet
+    
+    i = i + temp.substr(i, temp.find(';', i) - i).length() + 1;
+    flo = atoi(temp.substr(i, temp.find(';', i) - i).c_str()); // # of floors
+    
+    i = i + temp.substr(i, temp.find(';', i) - i).length() + 1;
+    yea = atoi(temp.substr(i, temp.find(';', i) - i).c_str()); // years
+    
+    i = i + temp.substr(i, temp.find(';', i) - i).length() + 1;
+    coor = temp.substr(i, temp.length() - i - 1); // coordinates
+    
+}
+
+
+
+
+/*******
+ menu display the menu.
+ let the user select from the menu.
+ *******/
+void menu(HASH*hash)
+{
+    
+    cout << " Hash-Table made COOL"<<endl;
+    cout<< "'S' Display content of the hash tabl"<<endl;
+    cout<< "'D' Display content of the hash tabl"<<endl;
+    cout<< "'P' Print the hash tabl"<<endl;
+    cout<< "'T' Show the statistics "<<endl;
+    cout<< "'M' Show Menu"<<endl;
+    cout<< "'Q' Quit"<<endl;
+    char choice;
+    
+    int userHeight=0;
+    
+    do
+    {
+        cout << "enter choice: ";
+        cin>>choice;
+        
+        
+        if(toupper(choice)=='S')
+        {
+            string name;
+            cout << "what are you looking for"<<endl;
+            cin.ignore();
+            getline(cin,name);
+            
+            Scrapper max=hash->search(name);
+            
+            if (max.empty())
+            {
+                cout << "not found"<< endl;
+                cin.ignore();
+                
+            }
+            else
+            {
+                cout << "How tall are you in feet?"<<endl;
+                cin >> userHeight;
+                cout << "found: ";
+                cout << max << endl;
+                
+                cout <<"It would take " << max.getheightft()/userHeight << " of you stacked from head to feet to be as tall as this tower"<<endl;
+            }
+        } // end if
+        
+        if(toupper(choice)=='D')
+        {
+            hash->print();
+        }
+        
+        if(toupper(choice)=='P')
+        {
+            hash->print_two();
+        }
+        
+        if(toupper(choice)=='T')
+        {
+            double load = hash->return_load();
+            double hashsize = hash->return_hashsize();
+            double collision = hash->return_collisions();
+        
+            cout << "there are " << hash->return_collisions() << " total collisions"<<endl;
+            cout << "load factor %" << 100*(load/hashsize)<<endl;
+            cout << "there are " << hash->return_buc_two() << " full buckets "<<endl;
+            cout << "there are " << hash->empty_buckets() << " empty buckets " << endl;
+            cout << "average number of nudes per bucket " << (collision/load) << endl;
+            
+            
+        }
+        
+        if(toupper(choice)=='M')
+        {
+            cout << " Hash-Table made COOL"<<endl;
+            cout<< "'S' Display content of the hash tabl"<<endl;
+            cout<< "'D' Display content of the hash tabl"<<endl;
+            cout<< "'P' Print the hash tabl"<<endl;
+            cout<< "'T' Show the statistics "<<endl;
+            cout<< "'M' Show Menu"<<endl;
+            cout<< "'Q' Quit"<<endl;
+        }
+        
+    }while(toupper(choice) !='Q');
+    
+}
+
+
+void menu_tree(BinarySearchTree<Scrapper> *tree1Ptr)
+{
+    char choice;
+    cout << "BST MENU"<< endl;
+    cout << "B: print tree by Breadth-First Traversal  " << endl;
+    cout << "D: print tree by Depth-First Traversal. (all 3 ways) " << endl;
+    cout << "I: print indented list " << endl;
+    cout << "S: Search tree by Unique Key " << endl;
+    cout << "R: Print range Tree in the Tree " << endl;
+    cout << "H: Help " << endl;
+    cout << "Q: Quit " << endl;
+    
+    do
+    {
+        cout<< "enter choice";
+        cin>>choice;
+        cin.ignore();
+        
+        
+        if (toupper(choice)=='B' )
+        {
+            cout << "breadth" << endl;
+            tree1Ptr->tree_breadth(display);
+        }
+        else if (toupper(choice)=='D' )
+        {
+            cout << "Inorder" << endl;
+            tree1Ptr->inOrder(display);
+            
+            cout << "postOrder" << endl;
+            tree1Ptr->postOrder(display);
+            
+            cout << "preOrder" << endl;
+            tree1Ptr->preOrder(display);
+        }
+        else if (toupper(choice)=='I' )
+        {
+            cout << "indented" << endl;
+            tree1Ptr->print_indented(display2);
+        }
+        else if (toupper(choice)=='R' )
+        {
+            int min;
+            int max;
+            cout << "Choice the range to print" << endl;
+            
+            cout<< "Enter the minimum number " << endl;
+            cin>>min;
+            cin.clear();
+            Scrapper low;
+            low.setheight(min);
+            
+            cout<< "Enter the maximum number " << endl;
+            cin>>max;
+            cin.clear();
+            Scrapper high;
+            high.setheight(max);
+            
+            tree1Ptr->range(display, low, high);
+        }
+        else if (toupper(choice)=='H')
+        {
+            cout << "select a letter to start."<< endl;
+            cout << "B: print tree by Breadth-First Traversal  " << endl;
+            cout << "D: print tree by Depth-First Traversal. (all 3 ways) " << endl;
+            cout << "I: print indented list " << endl;
+            cout << "S: Search tree by Secondary Key " << endl;
+            cout << "R: Print range Tree in a given Tree " << endl;
+            cout << "H: Help to show the menu " << endl;
+            cout << "Q: Quit " << endl;
+        }
+        else if (toupper(choice)=='A')
+        {
+            cout << "PikaPikaPikachu"<<endl;
+        }
+        else if (toupper(choice)=='S')
+        {
+            int height;
+            cout << "Search for a height"<< endl;
+            cin>> height;
+            
+            cin.ignore();
+            Scrapper search;
+            search.setheight(height);
+            Scrapper result;
+            if(tree1Ptr->getEntry(search, result))
+                cout <<result<<endl;
+            else
+                cout <<"No building found with that height"<<endl;
+        }
+        
+    }while(toupper(choice) !='Q');
+    cout << "PEACE OUT" <<endl;
+    
+    
+    
+}
+
